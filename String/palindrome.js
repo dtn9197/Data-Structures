d0e54ad6ab0cea941178cd7349b22bebc5223a15
Duy <dtn9197@gmail.com>
2020-08-29 14:50:43 -0400

    refactored palindrome

--- String/palindrome.js
+++ String/palindrome.js
@@ -1,10 +1,46 @@
 //write a function that takes a string and return a value if it is a palindrome
 //a palindrome is a word that when spells backward is the exact same thing
+
+/**use moving pointer pattern */
 function IsPalindrome(str) {
-    //conver string to lower case
+  let start = 0;
+  let end = str.length - 1;
+  while(start < end) {
+    let leftChar = str.charAt(start).toLowerCase();
+    let rightChar = str.charAt(end).toLowerCase();
+    if(leftChar !== rightChar)
+      return false;
+    start++;
+    end--;
+  }
+  return true;
+}
+
+  /** Algorithm
+   * create lowercase version of str
+   * create a reversed version of that str
+   * if the two strings are equal
+   *  return true
+   * else
+   *  false
+  */
+  function IsPalindrome2(str) {
+    var newString = str.toLowerCase();
+    var reversedString = newString.split("").reverse().join("");
+    if(newString == reversedString)
+      return true;
+    else
+      return "Not a palindrome";
+
+      //one liner solution
+    // return str === str.split('').reverse().join('');
+  }
+  
+
+  function IsPalindrome3(str) {
+   
     var lowercase = str.toLowerCase();
-    console.log(lowercase);
-    //find and store midpoint
+    
     var len = Math.floor(lowercase.length / 2);
     //loop from 0 to mid-point
     //if string at str[i] to str[i - 1 - i] not equals
@@ -15,13 +51,9 @@ function IsPalindrome(str) {
     }
     return true;
   }
-  
-  //API function for palindrome
-  function IsPalindrome2 (str) {
-    return str == str.split('').reverse().join('');
-  }
-  
-  console.log(IsPalindrome("Hannah"));
+  console.log(IsPalindrome("Hananah"));
   console.log(IsPalindrome2("Hannah"));
-  isP
+
+  
+  
   
